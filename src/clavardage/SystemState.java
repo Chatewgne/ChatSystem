f381c4bd9e3cc9def917bf205103191f55537b23
Ptiteigne <j-640@hotmail.fr>
2018-12-14 16:44:18 +0100

    Add an event generator to the system state, transmitting every change in the online user list

--- src/clavardage/SystemState.java
+++ src/clavardage/SystemState.java
@@ -6,11 +6,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class SystemState {
+public class SystemState implements UserListChangesEventGenerator{
 
     private int currentConversations;
+
     //private ArrayList<User> onlineUsers;
     private HashMap<String,User> onlineUsers;
+
+    // The object listening to the user list changes (notified here by the SystemState)
+    private ArrayList<UserListChangesListener> userListChangesListeners;
 
     public SystemState(){
         this.onlineUsers = new HashMap<String, User>();
@@ -24,15 +28,19 @@ public class SystemState {
     public void addOnlineUser(String adr,User us) {
         onlineUsers.put(adr, us);
         System.out.println("Added user : " + us.getID() + us.getUsername() + " available on host : " + adr );
+        this.reportNewUserListToListeners(onlineUsers);
     }
     public void removeOnlineUser(User us){
         onlineUsers.remove(us);
+        this.reportNewUserListToListeners(onlineUsers);
     }
     public void incrementConversationCount(){
         this.currentConversations++;
+        this.reportNewUserListToListeners(onlineUsers);
     }
     public void decrementConversationCount(){
         this.currentConversations--;
+        this.reportNewUserListToListeners(onlineUsers);
     }
 
     public String toString(){ //2:192.1.1.0:20181412184312:Ptiteigne:194.6.1.2:20180501123241:Chatewgne
@@ -49,5 +57,22 @@ public class SystemState {
         }
 
         return str;
+    }
+
+    @Override
+    public void addUserListChangesListener(UserListChangesListener listener) {
+        this.userListChangesListeners.add(listener);
+    }
+
+
+    // Give the new user list to every listeners of the SystemState
+    @Override
+    public void reportNewUserListToListeners(HashMap<String, User> onlineUsers) {
+
+        Iterator<UserListChangesListener> listeners = this.userListChangesListeners.iterator();
+
+        while(listeners.hasNext()){
+            listeners.next().userListHasChanged(onlineUsers);
+        }
     }
 }
---
+++ src/clavardage/UserListChangesEvent.java
@@ -0,0 +1,12 @@
+package clavardage;
+
+import java.util.HashMap;
+
+interface UserListChangesListener {
+    void userListHasChanged (HashMap<String,User> onlineUsers);
+}
+
+interface UserListChangesEventGenerator {
+    void addUserListChangesListener (UserListChangesListener listener);
+    void reportNewUserListToListeners(HashMap<String,User> onlineUsers);
+}
